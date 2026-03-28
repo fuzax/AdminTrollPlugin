5186dfa7459a8cdafe51ac8a58e02067285f8701
fuzax <simon.arbona@outlook.fr>
2026-03-28 21:19:06 +0100

    Create TrollManager.java file

---
+++ src/main/java/com/fuzax/plugin/managers/TrollManager.java
@@ -0,0 +1,12 @@
+// TrollManager.java
+
+package com.fuzax.plugin.managers;
+
+public class TrollManager {
+
+    // This class manages troll behavior
+
+    public void troll() {
+        // Implement trolling behavior here
+    }
+}
