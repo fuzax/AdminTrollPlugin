47a7c82beb8c607d6f070544a4b1dfb724ed67a0
fuzax <simon.arbona@outlook.fr>
2026-03-28 21:19:23 +0100

    Create PlayerListener.java file in listeners directory.

---
+++ src/main/java/com/fuzax/plugin/listeners/PlayerListener.java
@@ -0,0 +1,15 @@
+// PlayerListener.java
+
+package com.fuzax.plugin.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerListener implements Listener {
+    
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage("Welcome to the server!");
+    }
+}
