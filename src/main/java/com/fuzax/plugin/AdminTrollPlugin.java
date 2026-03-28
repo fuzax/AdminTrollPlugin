6136021d13918ef7d017b7e9b65880636d537aae
fuzax <simon.arbona@outlook.fr>
2026-03-28 21:18:39 +0100

    Create AdminTrollPlugin.java main class file

---
+++ src/main/java/com/fuzax/plugin/AdminTrollPlugin.java
@@ -0,0 +1,8 @@
+package com.fuzax.plugin;
+
+public class AdminTrollPlugin {
+    public static void main(String[] args) {
+        // TODO: Implement the main functionality of the plugin.
+        System.out.println("AdminTrollPlugin is running.");
+    }
+}
